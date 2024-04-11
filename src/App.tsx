77f21fbea645ec42aa7d21a36e529ce13a53080e
yipei <studentypz@gmail.com>
2024-04-11 14:25:56 +0200

    fix - bugs

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,29 @@ import Navbar from "./layouts/Navbar";
 
 function App() {
   return (
-    <div className="w-full min-h-screen">
+    <div className="w-max-screen min-h-screen">
       <header className="relative w-full">
         <Navbar />
       </header>
-      <Shape className="w-[100%] h-[70vh] top-[25%] -left-[50%]" type="Three" />
-      <Shape
-        className="w-[100%] h-[70vh] opacity-50 -top-[15%] -right-[50%]"
-        type="Two"
-      />
+
       <main className="min-h-sreen max-w-screen-xl mx-auto relative">
+        <Shape
+          className="w-[100%] h-[70%] top-[25%] -left-[50%]"
+          type="Three"
+        />
+        <Shape
+          className="hidden xl:block w-[100%] h-[70%] opacity-50 -top-[15%] -right-[50%]"
+          type="Two"
+        />
+        <Shape
+          className="hidden xl:block w-[100%] h-[70%] opacity-75 top-[140%] -right-[45%] rotate-[80deg]"
+          type="Two"
+        />
+
+        <Shape
+          className=" w-[100%] h-[70%] left-[-30%] opacity-75 top-[90%]  rotate-[80deg]"
+          type="One"
+        />
         <Hero />
         <About />
       </main>
